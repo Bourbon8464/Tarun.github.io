fc690dedc0fad1b3f4b42cb56231f52ba09f2ced
Bourbon8464 <98478187+Bourbon8464@users.noreply.github.com>
2025-11-15 02:03:39 +0000

    updated code

--- engineering-portfolio/vite.config.js
+++ engineering-portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Tarun.github.io/',
+  base: '/Engineering-Portfolio/',
   server: {
     proxy: {
       '/api': 'http://localhost:4000',
